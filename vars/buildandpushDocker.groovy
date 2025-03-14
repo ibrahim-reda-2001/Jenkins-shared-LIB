98ff10a2015076a516fc31d6f1e0d2777fd3e6b2
ibrahim-reda-2001 <ibrahimworkacount@gmail.com>
2025-03-14 21:46:53 +0200

    hello

--- vars/buildandpushDocker.groovy
+++ vars/buildandpushDocker.groovy
@@ -1,15 +1,11 @@
 #!/usr/bin/env groovy
-def call(String dockerCredentialsID, String repoName, String imageName, String dockerfile = 'Dockerfile' ) {
-    
-
-    // Define the full image name with tag (e.g., repoName/imageName:BUILD_NUMBER)
-    def fullImageName = "${repoName}/${imageName}:${BUILD_NUMBER}"
-
-    // Build the Docker image
+def call(String dockerCredentialsID, String repoName, String imageName, String dockerfile = 'Dockerfile') {
+    def fullImageName = "${repoName}/${imageName}:${env.BUILD_NUMBER}"
     def dockerImage = docker.build(fullImageName, "-f ${dockerfile} .")
-
-    // Push the image to the registry using credentials
-    withCredentials([usernamePassword(credentialsId: dockerCredentialsID, usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
+    
+    withCredentials([usernamePassword(credentialsId: dockerCredentialsID, 
+        usernameVariable: 'DOCKER_USERNAME', 
+        passwordVariable: 'DOCKER_PASSWORD')]) {
         sh """
             echo \$DOCKER_PASSWORD | docker login -u \$DOCKER_USERNAME --password-stdin
             docker push ${fullImageName}
