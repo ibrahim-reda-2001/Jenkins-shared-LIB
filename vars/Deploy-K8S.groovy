32666bdce01d67712207d27643adf13e7e635513
ibrahim-reda-2001 <ibrahimworkacount@gmail.com>
2025-03-14 22:39:18 +0200

    last

--- vars/Deploy-K8S.groovy
+++ vars/Deploy-K8S.groovy
@@ -1,9 +1,9 @@
 #!/usr/bin/env groovy
-def call(String k8sCredentialsID, String k8sApiServer, String namespace = 'default', string imageName) {
+def call(String k8sCredentialsID, String k8sApiServer, String namespace = 'default', String imageName) {
     withCredentials([string(credentialsId: k8sCredentialsID, variable: 'K8S_TOKEN')]) {
         sh """
-        sed -i 's|image: .*|image: ibrahimelmsery1/${imageName}|' deploy.yml
-            kubectl --token=\${K8S_TOKEN} \
+            sed -i 's|image: .*|image: ibrahimelmsery1/${imageName}|' deploy.yml
+            kubectl --token=\$K8S_TOKEN \
                     --server=${k8sApiServer} \
                     --insecure-skip-tls-verify \
                     -n ${namespace} \
--- vars/buildandpushDocker.groovy
+++ vars/buildandpushDocker.groovy
@@ -1,15 +1,18 @@
 #!/usr/bin/env groovy
 def call(String dockerCredentialsID, String repoName, String imageName, String dockerfile = 'Dockerfile') {
     def fullImageName = "${repoName}/${imageName}:${env.BUILD_NUMBER}"
-    def dockerImage = docker.build(fullImageName, "-f ${dockerfile} .")
     
+    script {
+        def dockerImage = docker.build(fullImageName, "-f ${dockerfile} .")
+    }
+
     withCredentials([usernamePassword(credentialsId: dockerCredentialsID, 
         usernameVariable: 'DOCKER_USERNAME', 
         passwordVariable: 'DOCKER_PASSWORD')]) {
         sh """
-            echo \$DOCKER_PASSWORD | docker login -u \$DOCKER_USERNAME --password-stdin
+            echo "\$DOCKER_PASSWORD" | docker login -u "\$DOCKER_USERNAME" --password-stdin
             docker push ${fullImageName}
-            docker rmi ${fullImageName}
+            docker rmi -f ${fullImageName}
         """
     }
 }
