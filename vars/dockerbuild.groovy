1db2882b39a93ac0f1e7829d53eb2c8b06a34f01
ibrahim_reda <128513557+ibrahim-reda-2001@users.noreply.github.com>
2025-03-15 04:33:46 +0200

    Update dockerbuild.groovy

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -1,9 +1,8 @@
-#!usr/bin/env groovy
 def call(String dockerHubCredentialsID, String repoName, String imageName) {
-    withCredentials([usernamePassword(credentialsId: "${dockerHubCredentialsID}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-        sh "docker login -u ${USERNAME} -p ${PASSWORD}"
-        sh "docker build -t ${repoName}/${imageName}:${BUILD_NUMBER} ."
-        sh "docker push ${repoName}/${imageName}:${BUILD_NUMBER}"    
-        sh "docker rmi ${repoName}/${imageName}:${BUILD_NUMBER}"
+    withCredentials([usernamePassword(credentialsId: dockerHubCredentialsID, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+        sh "echo '${PASSWORD}' | docker login -u '${USERNAME}' --password-stdin"
+        sh "docker build -t ${repoName}/${imageName}:${env.BUILD_NUMBER} ."
+        sh "docker push ${repoName}/${imageName}:${env.BUILD_NUMBER}"
+        sh "docker rmi ${repoName}/${imageName}:${env.BUILD_NUMBER}"
     }
 }
