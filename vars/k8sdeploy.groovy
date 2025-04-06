e0043e415df698e1e694efd8208be5a71782351a
ibrahim_reda <128513557+ibrahim-reda-2001@users.noreply.github.com>
2025-04-06 16:37:20 +0200

    Update k8sdeploy.groovy

--- vars/k8sdeploy.groovy
+++ vars/k8sdeploy.groovy
@@ -1,13 +1,18 @@
 #!/usr/bin/env groovy
-def call(String k8sCredentialsID, String k8sApiServer, String namespace = 'default', String imageName) {
+def call(String k8sCredentialsID, String k8sApiServer, String imageName, String namespace = 'default') {
     withCredentials([string(credentialsId: k8sCredentialsID, variable: 'K8S_TOKEN')]) {
-        sh """
-            sed -i 's|image: .*|image: ibrahimelmsery1/${imageName}|' deploy.yml
-            kubectl --token=\$K8S_TOKEN \
-                    --server=${k8sApiServer} \
-                    --insecure-skip-tls-verify \
-                    -n ${namespace} \
-                    apply -f deploy.yml
-        """
+        dir('Kubernetes') {  // Switch to Kubernetes directory
+            sh """
+                # Update image name in deployment.yml
+                sed -i 's|image: .*|image: ibrahimelmsery1/${imageName}:latest|' deployment.yml
+                
+                # Apply all Kubernetes manifests
+                kubectl --token=\$K8S_TOKEN \
+                        --server=${k8sApiServer} \
+                        --insecure-skip-tls-verify \
+                        -n ${namespace} \
+                        apply -f .
+            """
+        }
     }
 }
